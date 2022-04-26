660da3f6a8408e624a6ecb3748155617be863941
Suresh-Nakkeran <suresh.n@ideas2it.com>
2022-04-26 11:22:09 +0530

    Add basic valdidations in dataplane layer
    Signed-off-by: Suresh-Nakkeran <suresh.n@ideas2it.com>

--- python/kserve/kserve/handlers/dataplane.py
+++ python/kserve/kserve/handlers/dataplane.py
@@ -1,5 +1,9 @@
-from kserve.model_repository import ModelRepository
+import inspect
+
+import tornado.web
 from kserve.model import ModelType
+from kserve.model_repository import ModelRepository
+from ray.serve.api import RayServeHandle
 
 
 class DataPlane:
@@ -8,26 +12,37 @@ class DataPlane:
         self._model_registry = model_registry
 
     async def model_metadata(self, name):
-        # TODO: Make await optional for sync methods
         model = self._model_registry.get_model(name)
         return await model.metadata()
 
     async def explain(self, payload, name):
         model = self._model_registry.get_model(name)
         response = await model(payload, model_type=ModelType.EXPLAINER)
+        if not isinstance(model, RayServeHandle):
+            response = await model(payload, model_type=ModelType.EXPLAINER)
+        else:
+            model_handle = model
+            response = await model_handle.remote(payload, model_type=ModelType.EXPLAINER)
         return response
 
     async def infer(
         self, payload, name: str
     ):
         model = self._model_registry.get_model(name)
-        prediction = await model.predict(payload)
+        prediction = (await model.predict(payload)) if inspect.iscoroutinefunction(model.predict) \
+            else model.predict(payload)
         return prediction
 
     async def list(self):
         return {"models": list(self._model_registry.get_models().keys())}
 
     async def status(self, name):
+        model = self._model_registry.get_model(name)
+        if model is None:
+            raise tornado.web.HTTPError(
+                status_code=404,
+                reason="Model with name %s does not exist." % name
+            )
         is_ready = self._model_registry.is_model_ready(name)
         return {
             "name": name,
