7e1c567530f85582d66fabc05709c08e6557402b
Suresh-Nakkeran <suresh.n@ideas2it.com>
2022-08-02 19:10:28 +0530

    Install kourier as network layer istead of istio
    Signed-off-by: Suresh-Nakkeran <suresh.n@ideas2it.com>

--- hack/quick_install.sh
+++ hack/quick_install.sh
@@ -43,51 +43,53 @@ then
    exit 1;
 fi
 
-curl -L https://istio.io/downloadIstio | sh -
-cd istio-${ISTIO_VERSION}
+# curl -L https://istio.io/downloadIstio | sh -
+# cd istio-${ISTIO_VERSION}
 
-# Create istio-system namespace
-cat <<EOF | kubectl apply -f -
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: istio-system
-  labels:
-    istio-injection: disabled
-EOF
+# # Create istio-system namespace
+# cat <<EOF | kubectl apply -f -
+# apiVersion: v1
+# kind: Namespace
+# metadata:
+#   name: istio-system
+#   labels:
+#     istio-injection: disabled
+# EOF
 
-cat << EOF > ./istio-minimal-operator.yaml
-apiVersion: install.istio.io/v1beta1
-kind: IstioOperator
-spec:
-  values:
-    global:
-      proxy:
-        autoInject: disabled
-      useMCP: false
+# cat << EOF > ./istio-minimal-operator.yaml
+# apiVersion: install.istio.io/v1beta1
+# kind: IstioOperator
+# spec:
+#   values:
+#     global:
+#       proxy:
+#         autoInject: disabled
+#       useMCP: false
 
-  meshConfig:
-    accessLogFile: /dev/stdout
+#   meshConfig:
+#     accessLogFile: /dev/stdout
 
-  addonComponents:
-    pilot:
-      enabled: true
+#   addonComponents:
+#     pilot:
+#       enabled: true
 
-  components:
-    ingressGateways:
-      - name: istio-ingressgateway
-        enabled: true
-EOF
+#   components:
+#     ingressGateways:
+#       - name: istio-ingressgateway
+#         enabled: true
+# EOF
 
-bin/istioctl manifest apply -f istio-minimal-operator.yaml -y;
+# bin/istioctl manifest apply -f istio-minimal-operator.yaml -y;
 
-echo "😀 Successfully installed Istio"
+# echo "😀 Successfully installed Istio"
 
 # Install Knative
 if [ $deploymentMode = serverless ]; then
    kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-crds.yaml
    kubectl apply --filename https://github.com/knative/serving/releases/download/${KNATIVE_VERSION}/serving-core.yaml
-   kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
+  #  kubectl apply --filename https://github.com/knative/net-istio/releases/download/${KNATIVE_VERSION}/release.yaml
+   kubectl apply --filename https://github.com/knative/net-kourier/releases/download/${KNATIVE_VERSION}/kourier.yaml
+   kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
    echo "😀 Successfully installed Knative"
 fi
 
@@ -98,16 +100,16 @@ cd ..
 echo "😀 Successfully installed Cert Manager"
 
 # Install KServe
-KSERVE_CONFIG=kfserving.yaml
-if [ ${KSERVE_VERSION:3:1} -gt 6 ]; then KSERVE_CONFIG=kserve.yaml; fi
+# KSERVE_CONFIG=kfserving.yaml
+# if [ ${KSERVE_VERSION:3:1} -gt 6 ]; then KSERVE_CONFIG=kserve.yaml; fi
 
-# Retry inorder to handle that it may take a minute or so for the TLS assets required for the webhook to function to be provisioned
-kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/${KSERVE_CONFIG}
+# # Retry inorder to handle that it may take a minute or so for the TLS assets required for the webhook to function to be provisioned
+# kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/${KSERVE_CONFIG}
 
-# Install KServe built-in servingruntimes
-kubectl wait --for=condition=ready pod -l control-plane=kserve-controller-manager -n kserve --timeout=300s
-kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve-runtimes.yaml
-echo "😀 Successfully installed KServe"
+# # Install KServe built-in servingruntimes
+# kubectl wait --for=condition=ready pod -l control-plane=kserve-controller-manager -n kserve --timeout=300s
+# kubectl apply -f https://github.com/kserve/kserve/releases/download/${KSERVE_VERSION}/kserve-runtimes.yaml
+# echo "😀 Successfully installed KServe"
 
 # Clean up
 rm -rf istio-${ISTIO_VERSION}
